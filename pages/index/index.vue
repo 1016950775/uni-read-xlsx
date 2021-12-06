c42aed37376ce37b2b2cf0181ea0a843c8ffdb37
1016950775@qq.com <1016950775@qq.com>
2021-12-06 09:41:10 +0800

    first
    uniapp读取本地xlsxb表格数据

---
+++ pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png" @click="go"></image>
+		<view class="text-area">
+			<text class="title">js-xlsx:https://www.npmjs.com/package/js-xlsx</text>
+			<text>点击图片</text>
+			<!-- <excel @getResult="getMyExcelData1"></excel> -->
+		</view>
+
+		
+	</view>
+</template>
+
+<script>
+	import excel from "../../components/excelread.vue"
+	export default {
+		components:{
+			excel
+		},
+		data() {
+			return {
+				title: 'Hello',
+				src: ""
+			}
+		},
+		onLoad() {
+			
+		},
+		methods: {
+			handleMessage(e) {
+				console.log(e, "拿到的数据")
+			},
+			go(){
+				uni.navigateTo({
+					url:'../webview'
+				})
+			},
+			getMyExcelData1(data,datahtml){
+				console.log(data)
+				console.log(datahtml)
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		width: 100vw;
+		height: 100vh;
+	}
+
+	.web {
+		width: 100vw;
+		height: 200rpx;
+		background-color: red;
+	}
+</style>
---
+++ pages/webview.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="window">
+		<view class="cont" v-if="xldata" v-html="xldata">
+			
+		</view>
+		<web-view v-if="!xldata" :src="src" @message="handleMessage"></web-view>
+		<!-- <web-view src="http://192.168.1.23:8081/" @message="handleMessage"></web-view> -->
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				src:"",
+				xldata:""
+			}
+		},
+		onLoad() {
+			console.log(123)
+			setTimeout(()=>{
+				console.log(456)
+				this.src = "/hybrid/html/index.html"
+			},1000)
+		},
+		methods: {
+			handleMessage(e) {
+				console.log(e, "拿到的数据")
+				this.xldata  = e.detail.data[0].info
+			},
+		}
+	}
+</script>
+
+<style>
+.window{
+	width: 100vw;
+	height: 100vh;
+}
+</style>
